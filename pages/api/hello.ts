b40176ca752f2f49aa2061e29ebedc2fbff1ac5d
Chris Tate <chris@ctate.dev>
2023-10-04 15:02:21 -0500

    test screenshots

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,29 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import puppeteer from "puppeteer";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  try {
+    const browser = await puppeteer.launch({
+      headless: "new",
+    });
+    const page = await browser.newPage();
+    await page.goto("https://google.com");
+    await new Promise((res) => setTimeout(res, 2000));
+    await page.screenshot({ type: "png" });
+
+    console.log("good");
+  } catch (error) {
+    console.log("error");
+    console.log(error);
+  }
+
+  res.status(200).json({ name: "John Doe" });
 }
